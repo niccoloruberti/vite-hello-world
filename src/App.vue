81076e80dd4fd750f10fe40baa49a86adf11a134
niccoloruberti <nikiruberti@gmail.com>
2023-06-19 15:43:31 +0200

    esercizio base

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ export default {
 </script>
 
 <template>
-
+<AppTitle/>
 </template>
 
 <style scoped>
--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -5,9 +5,7 @@ export default {
 </script>
 
 <template>
-    <div>
-        
-    </div>
+<h1>La mia prima app con Vite!</h1>
 </template>
 
 <style>
