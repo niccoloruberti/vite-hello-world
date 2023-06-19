b818d3d89163fdd03640d513d7d94f51ca382ac3
niccoloruberti <nikiruberti@gmail.com>
2023-06-19 15:35:09 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import AppTitle from './components/AppTitle.vue'
+
+export default {
+  components: {
+    AppTitle
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
